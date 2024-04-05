72fbde764340417f044751cda908cb673a27dd5f
darlingson <dmakuwila@gmail.com>
2024-04-05 18:22:22 +0200

    added the controller for the ar

--- lib/Home.dart
+++ lib/Home.dart
@@ -1,5 +1,8 @@
+import 'package:arcore_flutter_plugin/arcore_flutter_plugin.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:vector_math/vector_math_64.dart' as vector;
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -9,12 +12,37 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  ArCoreController? augmentedRealityController;
+  augumentedRealityViewCreated(ArCoreController coreController) {
+    augmentedRealityController = coreController;
+    displayEarthMapSphere(augmentedRealityController);
+  }
+
+  displayEarthMapSphere(ArCoreController? coreController) async {
+    final ByteData bytes =
+        await rootBundle.load("images/Mulange_Massif_3D.gif");
+    final materials = ArCoreMaterial(
+      color: Color.fromARGB(120, 66, 134, 244),
+      textureBytes: bytes.buffer.asUint8List(),
+    );
+    final sphere = ArCoreSphere(
+      materials: [materials],
+      radius: 0.1,
+    );
+    final node = ArCoreNode(
+      shape: sphere,
+      position: vector.Vector3(0, 0, -1.5),
+    );
+    coreController?.addArCoreNode(node);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Home"),
-      )
+      ),
+      body: ArCoreView(onArCoreViewCreated: augumentedRealityViewCreated),
     );
   }
 }
