73a69ab0d4b053351c475db5e13c33108727d4eb
darlingson <dmakuwila@gmail.com>
2024-05-18 01:17:47 +0200

    managed to show earth using ar by following tutorial

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:malawi_virtual_tour_flutter/Home.dart';
+import 'earth_ar.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const MainApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const Home(),
-    );
+    return const MaterialApp(home: Earth_AR());
   }
 }
-
